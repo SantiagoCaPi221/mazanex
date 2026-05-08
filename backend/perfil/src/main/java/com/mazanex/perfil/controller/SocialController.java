1c0df428c991965e32c3ab47466ad8989d1b1427
brstockle <br.stockle@duocuc.cl>
2026-05-08 15:59:03 -0400

    Update SocialController.java

--- backend/perfil/src/main/java/com/mazanex/perfil/controller/SocialController.java
+++ backend/perfil/src/main/java/com/mazanex/perfil/controller/SocialController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/social")
+@RequestMapping("/api/perfil/social")
 @CrossOrigin(origins = "*")
 public class SocialController {
 
