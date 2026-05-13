9d7f9ee3e450764224fbba8c75d3d830d8c21bef
brstockle <br.stockle@duocuc.cl>
2026-05-12 22:43:20 -0400

    Update Notification.java

--- backend/microservicio_profile/profile/src/main/java/com/mazanex/perfil/model/Notification.java
+++ backend/microservicio_profile/profile/src/main/java/com/mazanex/perfil/model/Notification.java
@@ -8,25 +8,31 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "notifications")
 @Data
-@NoArgsConstructor
+@NoArgsConstructor // <--- Esto es lo que "realmente funciona" para Hibernate
 public class Notification {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "target_user_id") // Asegura el nombre de la columna en la DB
     private User targetUser;
 
     private String message;
     private String type; 
     private Long senderId; 
+    
     private boolean isRead = false;
     private LocalDateTime date = LocalDateTime.now();
 
+    // Constructor que usamos en el SocialService
     public Notification(User targetUser, String type, String message, Long senderId) {
         this.targetUser = targetUser;
         this.type = type;
         this.message = message;
         this.senderId = senderId;
+        this.isRead = false;
+        this.date = LocalDateTime.now();
     }
 }
