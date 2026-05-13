6a1479e50289b7feda0460255a7039f801098204
brstockle <br.stockle@duocuc.cl>
2026-05-13 16:45:13 -0400

    Update FriendRequest.java

--- backend/microservicio_profile/profile/src/main/java/com/mazanex/perfil/model/FriendRequest.java
+++ backend/microservicio_profile/profile/src/main/java/com/mazanex/perfil/model/FriendRequest.java
@@ -1,26 +1,23 @@
 package com.mazanex.profile.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "friend_requests")
-@Getter // Usamos Getter/Setter por separado para evitar errores de recursión
-@Setter
+@Data
 @NoArgsConstructor
 public class FriendRequest {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "sender_id")
     private User sender;
 
     @ManyToOne
-    @JoinColumn(name = "receiver_id")
     private User receiver;
 
     private String status;
