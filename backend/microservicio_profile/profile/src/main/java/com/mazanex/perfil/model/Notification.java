045675ad850eda8b3e44b048c6f8b5c2deffd3fa
brstockle <br.stockle@duocuc.cl>
2026-05-13 16:45:37 -0400

    Update Notification.java

--- backend/microservicio_profile/profile/src/main/java/com/mazanex/perfil/model/Notification.java
+++ backend/microservicio_profile/profile/src/main/java/com/mazanex/perfil/model/Notification.java
@@ -1,37 +1,33 @@
 package com.mazanex.profile.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "notifications")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor 
 public class Notification {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "target_user_id") 
     private User targetUser;
 
     private String message;
     private String type; 
-    private Long senderId; 
-    
     private boolean isRead = false;
     private LocalDateTime date = LocalDateTime.now();
 
-    // Constructor que usamos en el SocialService
-    public Notification(User targetUser, String type, String message, Long senderId) {
+    public Notification(User targetUser, String type, String message) {
         this.targetUser = targetUser;
         this.type = type;
         this.message = message;
-        this.senderId = senderId;
         this.isRead = false;
         this.date = LocalDateTime.now();
     }
