c661d437e6f7ca27c0ba33d6d05941d40622d4b0
nelu <nels.baeza@duocuc.cl>
2026-06-24 18:59:50 -0400

    update deployment.yml microservicios

--- backend/ms-auth/src/main/java/com/mazanex/auth/config/SwaggerConfig.java
+++ backend/ms-auth/src/main/java/com/mazanex/auth/config/SwaggerConfig.java
@@ -1,39 +1,20 @@
-package com.mazanex.auth.config; 
+package com.mazanex.auth.config;
 
-import com.mazanex.auth.security.JwtAuthenticationFilter;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
-
+public class SwaggerConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf(AbstractHttpConfigurer::disable) // Desactivamos CSRF porque usas JWT
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**").permitAll()
-                
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Mazanex Auth API")
+                        .version("1.0")
+                        .description("Microservicio encargado de la autenticación, emisión de tokens JWT y registro de usuarios.")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))/);
     }
 }
